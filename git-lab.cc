67e30c1d166f3f3f95f02f2cbeef6423926d48bc
cr1stelle <143845492+cr1stelle@users.noreply.github.com>
2023-09-11 16:00:49 -0400

    Create git-lab.cc

---
+++ git-lab.cc
@@ -0,0 +1,17 @@
+//
+//  File: git-lab.cc
+//   Author: Cristelle Gazave
+//   Date: 9/11/23
+//   Description: prints "Hello Git!!"
+//
+
+#include <iostream> 
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argc[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+
+}//main
